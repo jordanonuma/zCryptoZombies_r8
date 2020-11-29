68037f19eec173826d40f76c62cac38a67c80cae
jordanonuma <jordanonuma@outlook.com>
2020-11-29 11:59:18 -1000

    Added newly-created zombie to the 'zombies' array.

--- Contract.sol
+++ Contract.sol
@@ -15,8 +15,7 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
-        uint id = zombies.push() - 1;
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
     } //end function _createZombie()
 
