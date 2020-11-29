4902e68c7d5bb4ed441f32e21830ede71f50b79e
jordanonuma <jordanonuma@outlook.com>
2020-11-29 11:56:55 -1000

    Fired notification to front end using just-defined zombie ID.

--- Contract.sol
+++ Contract.sol
@@ -17,6 +17,7 @@ contract ZombieFactory {
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
         uint id = zombies.push() - 1;
+        emit NewZombie(id, _name, _dna);
     } //end function _createZombie()
 
     function _generateRandomDna(string memory _str) private view  returns (uint) {
