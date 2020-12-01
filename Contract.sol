f3d93ea0c6398c40675d78f655fd030818d9bee8
jordanonuma <jordanonuma@outlook.com>
2020-11-30 18:44:30 -1000

    Added mapping to find the owner.

--- Contract.sol
+++ Contract.sol
@@ -13,6 +13,7 @@ contract ZombieFactory {
     } //end struct Zombie{}
 
     Zombie[] public zombies;
+    mapping (uint => address) public zombieToOwner;
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
