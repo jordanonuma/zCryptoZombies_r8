c5caf51b8c1434b0ea027db0449f236adf6e21c8
jordanonuma <jordanonuma@outlook.com>
2020-12-01 19:29:56 -1000

    Stores the zombie ID under the user's address.

--- Contract.sol
+++ Contract.sol
@@ -18,6 +18,7 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[msg.sender] == id;
         emit NewZombie(id, _name, _dna);
     } //end function _createZombie()
 
