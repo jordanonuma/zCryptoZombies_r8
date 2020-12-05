2e1ba8dcef21ad2ef3e8eb1b90e80f1cb38cfc31
jordanonuma <jordanonuma@outlook.com>
2020-12-05 09:33:51 -1000

    Checks user owns zombie.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,7 +4,7 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
     
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-      
+        require(msg.sender == zombieToOwner[_zombieId]);
     } //end function feedAndMultiply()
 
 } //end contract ZombieFeeding{}
