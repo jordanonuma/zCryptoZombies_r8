a1cee7a9888d39b571bc37e56300901a2521ec14
jordanonuma <jordanonuma@outlook.com>
2020-12-05 09:36:04 -1000

    Declared a local storage of Zombie[] struct named 'myZombie'.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,6 +5,7 @@ contract ZombieFeeding is ZombieFactory {
     
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
     } //end function feedAndMultiply()
 
 } //end contract ZombieFeeding{}
