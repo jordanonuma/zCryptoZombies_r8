c8e5f2060dc7bff1d102e5362805a56a51f0bfaf
jordanonuma <jordanonuma@outlook.com>
2020-12-06 11:43:15 -1000

    Used the attackee's DNA.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -8,7 +8,7 @@ contract ZombieFeeding is ZombieFactory {
         Zombie storage myZombie = zombies[_zombieId];
 
         _targetDna = _targetDna % dnaModulus;
-        uint newDna = (myZombie.dna + newDna) / 2;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
         _createZombie("NoName", newDna);
     } //end function feedAndMultiply()
 
