5d7a5dd904c71827d5eecd4f44dbe81849c1d1f3
jordanonuma <jordanonuma@outlook.com>
2020-12-19 09:56:37 -1000

    Added check that zombie meets the contract owner's defined level requirement.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,6 +4,6 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
     modifier aboveLevel(uint _level, uint _zombieId) {
-    
+        require(zombies[_zombieId].level >= _level);
     } //end modifier aboveLevel()
 } //end contract ZombieHelper{}
