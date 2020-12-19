d6c81016b915a1dc568b6fb4eedd6423d8fda74d
jordanonuma <jordanonuma@outlook.com>
2020-12-19 09:57:10 -1000

    Added return back to function being modified.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -5,5 +5,6 @@ import "./zombiefeeding.sol";
 contract ZombieHelper is ZombieFeeding {
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
+        _;
     } //end modifier aboveLevel()
 } //end contract ZombieHelper{}
