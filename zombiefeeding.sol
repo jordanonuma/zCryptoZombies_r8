348f493d614d13edb5f5486ffa34cf81378db24f
jordanonuma <jordanonuma@outlook.com>
2020-12-18 20:18:20 -1000

    Revised function to internal so that only other functions within contract and inherited contracts can call it.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -33,7 +33,7 @@ contract ZombieFeeding is ZombieFactory {
         return (_zombie.readyTime <= now);
     } //end function _isReady()
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
 
