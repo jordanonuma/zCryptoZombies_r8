2f2dc03c0f56a3a12d86ee18c962864cfa4f286b
jordanonuma <jordanonuma@outlook.com>
2020-12-20 09:42:24 -1000

    Added frame of function used to change zombie DNA. Zombie must be level 20 or greater.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -12,4 +12,8 @@ contract ZombieHelper is ZombieFeeding {
         require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].name = _newName;
     } //end function changeName()
+
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    
+    } //end function changeDna()
 } //end contract ZombieHelper{}
