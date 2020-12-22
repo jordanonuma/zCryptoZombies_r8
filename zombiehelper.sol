b18d8a450fa265488ed8fc8e761b634311c896ff
jordanonuma <jordanonuma@outlook.com>
2020-12-22 09:27:46 -1000

    Sets the length of the array equal to how many zombies the user owns.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -19,6 +19,6 @@ contract ZombieHelper is ZombieFeeding {
     } //end function changeDna()
 
     function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
-        uint[] memory result;
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
     } //end function getZombiesByOwner()
 } //end contract ZombieHelper{}
