bb87f8cfc9f2b78ebe4ce5db68c4d2b12c61b048
jordanonuma <jordanonuma@outlook.com>
2020-12-24 14:54:11 -1000

    If checks are passed the zombie's level is incremented.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -12,6 +12,7 @@ contract ZombieHelper is ZombieFeeding {
     
     function levelUp(uint _zombieId) external payable {
         require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
     } //end function levelUp()
 
     function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
