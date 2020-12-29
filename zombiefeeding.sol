ff3a986a2834662c5d47cee5e37b4f88ec61edd7
jordanonuma <jordanonuma@outlook.com>
2020-12-29 09:20:37 -1000

    Added modifier that checks user owns zombie.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -21,6 +21,12 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     
     KittyInterface kittyContract;
+
+    modifier ownerOf(uint _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
+    } //end modifier ownerOf()
+    
     function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     } //end function setKittyContractAddress()
