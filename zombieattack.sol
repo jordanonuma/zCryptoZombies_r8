bfc6bfd977eebdb158bae95c71ad56f003ef78d0
jordanonuma <jordanonuma@outlook.com>
2020-12-31 10:19:52 -1000

    Pseudorandom number generated for winning probability.

--- zombieattack.sol
+++ zombieattack.sol
@@ -13,5 +13,6 @@ contract ZombieAttack is ZombieHelper {
     function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
     } //end function attack()
 } //end contract ZombieAttack{}
