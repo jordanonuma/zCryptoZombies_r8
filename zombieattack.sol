5e8c3b9a0339090ecdf793ffe63f24db2ef34411
jordanonuma <jordanonuma@outlook.com>
2021-01-03 09:42:22 -1000

    Added tallies for loss case.

--- zombieattack.sol
+++ zombieattack.sol
@@ -21,6 +21,10 @@ contract ZombieAttack is ZombieHelper {
             enemyZombie.lossCount++;
 
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
-        } //end if()
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+      
+        }//end if()
     } //end function attack()
 } //end contract ZombieAttack{}
