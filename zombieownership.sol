0270174382c50ea4819b0459e4b642a84e37c103
jordanonuma <jordanonuma@outlook.com>
2021-01-06 06:30:58 -1000

    Added frame of function that returns user's balance.

--- zombieownership.sol
+++ zombieownership.sol
@@ -3,5 +3,9 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    
+    function balanceOf(address _owner) external view returns (uint256) {
+    
+    } //end function balanceOf()
 
 } //end contract ZombieOwnership{}
