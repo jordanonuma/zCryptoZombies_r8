50d3bf361d3112542269d6ed0d1683b30c50055c
jordanonuma <jordanonuma@outlook.com>
2021-01-09 08:24:19 -1000

    Calls function to transfer.

--- zombieownership.sol
+++ zombieownership.sol
@@ -23,6 +23,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
         require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
     } //end function transferFrom()
 
     function approve(address _approved, uint256 _tokenId) external payable {
