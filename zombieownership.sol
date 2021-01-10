61461726ab78fcd0092731a2dacf6f9bf60f3e3d
jordanonuma <jordanonuma@outlook.com>
2021-01-10 10:18:01 -1000

    Added the approved address.

--- zombieownership.sol
+++ zombieownership.sol
@@ -27,7 +27,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     } //end function transferFrom()
 
     function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-
+        zombieApprovals[_tokenId] = _approved;
     } //end function approve()
 
 } //end contract ZombieOwnership{}
