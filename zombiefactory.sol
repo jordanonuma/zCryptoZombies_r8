07385d0ac3e2c968e15f20ca4f4005d3a825e2a7
jordanonuma <jordanonuma@outlook.com>
2021-01-15 06:32:18 -1000

    Declared using SafeMath for uint16 and uint32 types.

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -5,6 +5,8 @@ import "./safemath.sol";
 contract ZombieFactory is Ownable {
 
     using SafeMath for uint256;
+    using SafeMath for uint32;
+    using SafeMath for uint16;
     
     event NewZombie(uint zombieId, string name, uint dna);
 
