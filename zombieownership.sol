10e2c330e04e03b415651e7ef40ec26720d7ff24
jordanonuma <jordanonuma@outlook.com>
2021-01-17 09:27:19 -1000

    Practicing natspec documentation with title, author, and dev parameters.

--- zombieownership.sol
+++ zombieownership.sol
@@ -2,6 +2,9 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombieattack.sol";
 import "./erc721.sol";
 
+/// @title a contract tracks zombie ownership
+/// @author cryptozombies.io tutorial in conjuction with me
+/// @dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
 contract ZombieOwnership is ZombieAttack, ERC721 {
     
     mapping (uint => address) zombieApprovals;
