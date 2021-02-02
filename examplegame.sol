237d38e64433203a8ae1be740b38f672f62460dc
jordanonuma <jordanonuma@outlook.com>
2021-02-01 18:06:05 -1000

    Fills the starting 3 goo vials for both players.

--- examplegame.sol
+++ examplegame.sol
@@ -16,6 +16,10 @@ contract ExampleGame is ZBGameMode  {
 
         changes.changePlayerCurrentGooVials(Player.Player1, 3);
         changes.changePlayerCurrentGooVials(Player.Player2, 3);
+
+        //Number of filled goo vials
+        changes.changePlayerCurrentGoo(Player.Player1, 3):
+        changes.changePlayerCurrentGoo(Player.Player2, 3):
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
