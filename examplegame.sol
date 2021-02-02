ff97c43602706333f9524721a7472eb32f906783
jordanonuma <jordanonuma@outlook.com>
2021-02-01 18:07:30 -1000

    Sets both players' max number of vials to 8.

--- examplegame.sol
+++ examplegame.sol
@@ -13,13 +13,17 @@ contract ExampleGame is ZBGameMode  {
 
         changes.changePlayerDefense(Player.Player1, 15);
         changes.changePlayerDefense(Player.Player2, 15);
-
+        
         changes.changePlayerCurrentGooVials(Player.Player1, 3);
         changes.changePlayerCurrentGooVials(Player.Player2, 3);
 
         //Number of filled goo vials
         changes.changePlayerCurrentGoo(Player.Player1, 3):
         changes.changePlayerCurrentGoo(Player.Player2, 3):
+
+        //Max number of vials
+        changes.chanePlayerMaxGooVials(Player.Player1, 8):
+        changes.chanePlayerMaxGooVials(Player.Player2, 8):
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
