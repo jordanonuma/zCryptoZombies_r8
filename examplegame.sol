baad89117a189eddce3f2a6485bd789607fe290b
jordanonuma <jordanonuma@outlook.com>
2021-02-06 10:38:43 -1000

    Corrected variable name for consistency.

--- examplegame.sol
+++ examplegame.sol
@@ -65,7 +65,7 @@ contract ExampleGame is ZBGameMode  {
                     cardCount++;
                 } //end if()
             } //end for()
-            changes.changePlayerCardsInDeck(Player(i), newCards, CardCount); //swaps out old cards for the approved cards in array newCards[]
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount); //swaps out old cards for the approved cards in array newCards[]
         } //end for()
     } //end function beforeMatchStart()
 
