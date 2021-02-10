513d7a84742b1e2a807e7e8fbcf55f1f57cf22c0
jordanonuma <jordanonuma@outlook.com>
2021-02-09 17:37:12 -1000

    Copied nested for() loops that check each player's cards are playable.

--- munchkin.sol
+++ munchkin.sol
@@ -13,6 +13,19 @@ contract Munchkin is ZBGameMode  {
         changes.init();
 
         // Custom game logic will go here
+        for (uint i = 0; i < gameState.playerStates.length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
+
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                } //end if()
+            } //end for()
+
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
+        } //end for()
 
         changes.emit();
 
