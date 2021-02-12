819ae75ccd1a8aef7a4283580aaf222e02c1cbc1
jordanonuma <jordanonuma@outlook.com>
2021-02-11 18:08:30 -1000

    Set boolean check to false.

--- singleton.sol
+++ singleton.sol
@@ -18,6 +18,7 @@ contract Singleton is ZBGameMode  {
             uint cardCount = 0;
 
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                bool cardAlreadyInDeck = false;
                 if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
                     newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
                     cardCount++;
