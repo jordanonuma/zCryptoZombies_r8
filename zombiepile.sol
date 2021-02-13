69e792f2d1e034bad98ebdc54cee72b68eb85296
jordanonuma <jordanonuma@outlook.com>
2021-02-13 10:32:53 -1000

    Templated from Munchkin's contract, changing contract frame.

---
+++ zombiepile.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.4.25;
+
+import "./ZB/ZBGameMode.sol";
+
+contract ZombiePile is ZBGameMode  {
+
+    function beforeMatchStart(bytes serializedGameState) external {
+
+        GameState memory gameState;
+        gameState.init(serializedGameState);
+
+        ZBSerializer.SerializedGameStateChanges memory changes;
+        changes.init();
+
+        // Custom game logic will go here
+        for (uint i = 0; i < gameState.playerStates.length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
+
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                } //end if()
+            } //end for()
+
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
+        } //end for()
+
+        changes.emit();
+
+        function isLegalCard(CardInstance card) internal view returns(bool) {
+            return (card.gooCost <= 2);
+        } //end function isLegalCard()
+
+    } //end function beforeMatchStart()
+
+} //end contract ZombiePile{}
