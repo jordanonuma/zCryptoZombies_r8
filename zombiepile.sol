d49ab8f9362ea67c432d99897c1ff40a7d7c58fd
jordanonuma <jordanonuma@outlook.com>
2021-02-14 09:40:35 -1000

    Similarly set the player 2's cards and incremented their card counter.

--- zombiepile.sol
+++ zombiepile.sol
@@ -26,7 +26,8 @@ contract ZombiePile is ZBGameMode  {
                     player1Cards[player1CardCount] = gameState.playerStates[i].cardsInDeck[j];
                     player1CardCount++;
                 } else {
-                    
+                    player2Cards[player2CardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    player2CardCount++;
                 } //end if-else()
             } //end for()
 
