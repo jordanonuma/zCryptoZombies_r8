a754111e31de4e17ce772bba98fac0584a59f570
jordanonuma <jordanonuma@outlook.com>
2021-02-20 09:59:49 -1000

    Revised spelling.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,4 +1,4 @@
-const CryptoZombies = artifacts.require("CryptoZombes"); //pulling from build folder is being simulated but not actually used in this practice session
+const CryptoZombies = artifacts.require("CryptoZombies"); //pulling from build folder is being simulated but not actually used in this practice session
 
 contract("CryptoZombies", (accounts) => {
     it("should be able to create a new zombie", () => {
