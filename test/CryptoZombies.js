1a309c8676d8c82bc43ddd0cc70f35a0090252ff
jordanonuma <jordanonuma@outlook.com>
2021-02-21 06:49:38 -1000

    Initialized accounts to alice and bob.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,6 +1,7 @@
 const CryptoZombies = artifacts.require("CryptoZombies"); //pulling from build folder is being simulated but not actually used in this practice session
 
 contract("CryptoZombies", (accounts) => {
+    let [alice, bob] = accounts;
     it("should be able to create a new zombie", () => {
 
     })
