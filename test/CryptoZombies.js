e800542a77051d6459ca48e78ad144755b295665
jordanonuma <jordanonuma@outlook.com>
2021-02-23 20:34:11 -1000

    Added await for asynchronous transactions and corrected variable name for consistency.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -4,8 +4,8 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
-        const result = contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         assert.equal(result.receipt.status, true);
-        assert.equal(result.logs[0].args.name, zombiesNames[0]);
+        assert.equal(result.logs[0].args.name, zombieNames[0]);
     })
 }) //end contract{}
