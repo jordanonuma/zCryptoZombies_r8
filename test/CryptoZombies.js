56dfceb605a3b7e613762253c72818313ddb4a1a
jordanonuma <jordanonuma@outlook.com>
2021-02-23 20:30:18 -1000

    Calls the solidity function createRandomZombie() storing msg.sender as 'alice'.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -4,5 +4,6 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
+        const result = contractInstance.createRandomZombie(zombieNames[0], {from: alice});
     })
 }) //end contract{}
