ed5e78d5c19cd8b4da6214fc3979c49ef6e5ade9
jordanonuma <jordanonuma@outlook.com>
2021-02-24 17:01:34 -1000

    Removed redundant declaration of contract instance. Revised spelling.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -9,13 +9,12 @@ contract("CryptoZombies", (accounts) => {
     });
 
     it("should be able to create a new zombie", async () => {
-        const contractInstance = await CryptoZombies.new();
         const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.name, zombieNames[0]);
     })
 
-    it("shold not allow two zombies", async () => {
+    it("should not allow two zombies", async () => {
         
     })
 }) //end contract{}
