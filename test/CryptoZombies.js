2bfa5839c9618205fa46f93783177b0214c78d73
jordanonuma <jordanonuma@outlook.com>
2021-02-28 10:15:51 -1000

    Unflagged so the test will run.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -19,7 +19,7 @@ contract("CryptoZombies", (accounts) => {
         await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
     })
 
-    xcontext("with the single-step transfer scenario", async () => {
+    context("with the single-step transfer scenario", async () => {
         it("should transfer a zombie", async () => {
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
