e8609399e4a884b7db739d07ebe5d7f0d051af1f
jordanonuma <jordanonuma@outlook.com>
2021-03-03 19:46:13 -1000

    Increases EVM's time so that cooldown doesn't cause attack to fail.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -55,6 +55,7 @@ contract("CryptoZombies", (accounts) => {
         result = await contractInstance.createRandomZombie(zombieNames[1], {from: bob});
         const secondZombieId = result.logs[0].args.zombieId.toNumber();
         
+        await time.increase(time.duration.days(1));
         await contractInstance.attack(firstZombieId, secondZombieId, {from: alice});
         assert.equal(result.receipt.status, true);
     })
