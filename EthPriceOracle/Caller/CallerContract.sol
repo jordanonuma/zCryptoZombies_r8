10aec35fa40fa0d326e4161befed4f798aa9a13c
jordanonuma <jordanonuma@outlook.com>
2021-03-06 09:39:41 -1000

    Sets address.

--- EthPriceOracle/Caller/CallerContract.sol
+++ EthPriceOracle/Caller/CallerContract.sol
@@ -4,6 +4,6 @@ contract CallerContract {
     address private oracleAddress;
 
     function setOracleInstanceAddress(address _oracleInstanceAddres) public {
-        
+        oracleAddress = _oracleInstanceAddress;
     } //end function setOracleInstanceAddress()
 } //end contract CallerContract{}
