dbd1be1d0374c5a33f87bdc924bec21a938fdac7
jordanonuma <jordanonuma@outlook.com>
2021-03-06 09:38:56 -1000

    Added frame of function that sets the oracle address.

--- EthPriceOracle/Caller/CallerContract.sol
+++ EthPriceOracle/Caller/CallerContract.sol
@@ -2,4 +2,8 @@ pragma solidity 0.5.0;
 
 contract CallerContract {
     address private oracleAddress;
+
+    function setOracleInstanceAddress(address _oracleInstanceAddres) public {
+        
+    } //end function setOracleInstanceAddress()
 } //end contract CallerContract{}
