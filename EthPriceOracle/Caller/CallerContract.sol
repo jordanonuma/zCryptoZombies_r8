d59fc1904594428caffa9712bad55d62ad1ec975
jordanonuma <jordanonuma@outlook.com>
2021-03-07 10:19:03 -1000

    Import interfacing contract and declares the EthPriceOracleInterface type variable called oracleInstance.

--- EthPriceOracle/Caller/CallerContract.sol
+++ EthPriceOracle/Caller/CallerContract.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.5.0;
+import "./EthPriceOracleInterface.sol";
 
 contract CallerContract {
+    EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
 
     function setOracleInstanceAddress(address _oracleInstanceAddres) public {
