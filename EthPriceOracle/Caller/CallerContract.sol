0a25420d08603f8a075748fd3ce0c55d768172b9
jordanonuma <jordanonuma@outlook.com>
2021-03-10 17:19:50 -1000

    Defined event and mapping that shows whether a request has been received.

--- EthPriceOracle/Caller/CallerContract.sol
+++ EthPriceOracle/Caller/CallerContract.sol
@@ -6,6 +6,9 @@ contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
+
+    mapping(uint256=>bool) myRequests;
 
     function setOracleInstanceAddress(address _oracleInstanceAddres) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
