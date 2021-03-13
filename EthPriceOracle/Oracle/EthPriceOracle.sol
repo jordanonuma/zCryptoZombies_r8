47fa520b67f52b22a4e2fe0e63367f00f9f94c72
jordanonuma <jordanonuma@outlook.com>
2021-03-13 08:54:42 -1000

    Increments nonce.

--- EthPriceOracle/Oracle/EthPriceOracle.sol
+++ EthPriceOracle/Oracle/EthPriceOracle.sol
@@ -3,13 +3,13 @@ import "./../../Ownable.sol";
 import "./../Caller/CallerContractInterface.sol";
 
 contract EthPriceOracle is Ownable {
-  uint private randNonce = 0;
-  uint private modulus = 1000;
-  mapping(uint256=>bool) pendingRequests;
-  event GetLatestEthPriceEvent(address callerAddress, uint id);
-  event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    uint private randNonce = 0;
+    uint private modulus = 1000;
+    mapping(uint256=>bool) pendingRequests;
+    event GetLatestEthPriceEvent(address callerAddress, uint id);
+    event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
   
-  function getLatestEthPrice() public returns(uint256) {
-      
-  } //end function getLatestEthPrice()
+    function getLatestEthPrice() public returns(uint256) {
+        randNonce++;
+    } //end function getLatestEthPrice()
 } //end contract EthPriceOracle{}
