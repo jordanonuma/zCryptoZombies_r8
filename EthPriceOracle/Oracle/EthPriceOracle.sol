b339c090ab05611ad0876dd2d2b4efa99230f693
jordanonuma <jordanonuma@outlook.com>
2021-03-14 10:15:14 -1000

    Revised case for variable consistency.

--- EthPriceOracle/Oracle/EthPriceOracle.sol
+++ EthPriceOracle/Oracle/EthPriceOracle.sol
@@ -12,7 +12,7 @@ contract EthPriceOracle is Ownable {
     function getLatestEthPrice() public returns(uint256) {
         randNonce++;
         uint id = uint(keccak256(abi.encodePacked(now, msg.sender,randNonce))) % modulus;
-        pendingrequests[id] = true;
+        pendingRequests[id] = true;
         emit GetLatestEthPriceEvent(msg.sender, id);
         return id;
     } //end function getLatestEthPrice()
