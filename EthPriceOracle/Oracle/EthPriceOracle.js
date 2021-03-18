e1c1c6d65e61b792adaa180f5f5fde21bba7f893
jordanonuma <jordanonuma@outlook.com>
2021-03-17 18:30:18 -1000

    Added JS function that identified which network the contract has been deployed and what the build artifacts are.

--- EthPriceOracle/Oracle/EthPriceOracle.js
+++ EthPriceOracle/Oracle/EthPriceOracle.js
@@ -5,5 +5,10 @@ const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 2000
 const PRIVATE_KEY_FILE_NAME = process.env.PRIVATE_KEY_FILE || './oracle/oracle_private_key'
 const CHUNK_SIZE = process.env.CHUNK_SIZE || 3
 const MAX_RETRIES = process.env.MAX_RETRIES || 5
-const OracleJSON = require('./oracle/build/contracts/EthPriceOracle.json')
+const OracleJSON = require('./EthPriceOracle.json')
 var pendingRequests = []
+
+async function getOracleContract(web3js) {
+    await const networkId = await web3js.eth.net.getId()
+    return const oracleJSON
+} //end function getOracleContract()
