7c7aa293662982bf4c5106bd26e206a03db14add
jordanonuma <jordanonuma@outlook.com>
2021-03-26 19:56:15 -1000

    Removes decimal.

--- EthPriceOracle/Oracle/EthPriceOracle.js
+++ EthPriceOracle/Oracle/EthPriceOracle.js
@@ -63,6 +63,7 @@ async function processRequest (oracleContract, ownerAddress, id, callerAddress) 
 
 async function setLatestEthPrice (oracleContract, callerAddress, ownerAddress, ethPrice, id) {
     // Start here
+    ethPrice = ethPrice.replace('.','')
     const ethPriceInt = (new BN(parseInt(ethPrice), 10)).mul(multiplier)
     const idInt = new BN(parseInt(id))
     try {
