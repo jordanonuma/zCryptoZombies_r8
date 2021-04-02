3a62cb45873ed3ac76accff1e5c9676178088c0a
jordanonuma <jordanonuma@outlook.com>
2021-04-02 10:37:32 -1000

    Revised for the oracles role.

--- EthPriceOracle/Oracle/EthPriceOracle.sol
+++ EthPriceOracle/Oracle/EthPriceOracle.sol
@@ -22,7 +22,7 @@ contract EthPriceOracle {
     function addOracle(address _oracle) public {
         require(owners.has(msg.sender), "Not an owner!");
         require(!oracles.has(_oracle), "Already an oracle!");
-        oracle.add(_oracle);
+        oracles.add(_oracle);
         emit AddOracleEvent(_oracle);
     } //end function addOracle()
 
