30d49e844b26605e0993158d098de5ee987bb7c8
jordanonuma <jordanonuma@outlook.com>
2021-04-04 09:35:19 -1000

    Defined struct Response{}.

--- EthPriceOracle/Oracle/EthPriceOracle.sol
+++ EthPriceOracle/Oracle/EthPriceOracle.sol
@@ -14,7 +14,12 @@ contract EthPriceOracle {
 
     mapping(uint256=>bool) pendingRequests;
     mapping (uint256=>Response[]) public requestIdToResponse;
-    
+    struct Response {
+        address oracleAddress;
+        address callerAddress;
+        uint256 ethPrice;
+    } //end  struct Response {}
+
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
     event AddOracleEvent(address oracleAddress);
