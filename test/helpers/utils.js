fd24f9bcf07f4df98e268505e9964c9b5210b3d9
jordanonuma <jordanonuma@outlook.com>
2021-04-09 19:05:14 -1000

    Added try-catch block for errors.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,5 +1,14 @@
 async function getZkSyncProvider(zksync, networkName) {
     let zkSyncProvider
+
+    try {
+        // Initialize the provider
+        await zksync.getDefaultProvider(networkName)
+    } catch (error) {
+        // Display an error message
+        console.log('Unable to connect to zkSync.')
+        console.log(error)
+    }
 } //end async function getZkSyncProvider()
 
 async function shouldThrow(promise) {
