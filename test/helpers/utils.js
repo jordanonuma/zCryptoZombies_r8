c4903b414c856324ada541c9ed885a943e79d650
jordanonuma <jordanonuma@outlook.com>
2021-04-09 19:02:46 -1000

    Added frame of function that calls zksync provider.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,3 +1,7 @@
+async function getZkSyncProvider(zksync, networkName) {
+    let zkSyncProvider
+} //end async function getZkSyncProvider()
+
 async function shouldThrow(promise) {
     try {
         await promise;
