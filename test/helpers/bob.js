a7e30bac8565afa7b9fe28308aef0d4a123947c1
jordanonuma <jordanonuma@outlook.com>
2021-04-18 10:40:08 -1000

    Added await for async call.

--- test/helpers/bob.js
+++ test/helpers/bob.js
@@ -3,7 +3,7 @@
     const zksync = require('zksync')
     const utils = require('./utils')
   
-    export NETWORK_NAME = rinkeby
-    const zkSyncProvider = utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
-    const ethersProvider = utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
+    // export NETWORK_NAME = rinkeby may be necessary before running Node.JS app
+    const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+    const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
 })()
