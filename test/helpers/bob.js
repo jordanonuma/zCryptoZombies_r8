c142ef5b82b24b9e764a416ad5469ea37826c53c
jordanonuma <jordanonuma@outlook.com>
2021-04-18 10:36:53 -1000

    Created zkSync provider.

---
+++ test/helpers/bob.js
@@ -0,0 +1,8 @@
+(async () => {
+    const ethers = require('ethers')
+    const zksync = require('zksync')
+    const utils = require('./utils')
+  
+    export NETWORK_NAME = rinkeby
+    const zkSyncProvider = utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+})()
