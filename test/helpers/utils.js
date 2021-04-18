2f407ad86e1549a7427fad3761baef75b3af7003
jordanonuma <jordanonuma@outlook.com>
2021-04-18 10:23:53 -1000

    Specifies which variables and functions Node.js will export from this JS file.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -119,3 +119,15 @@ async function displayZkSyncBalance (wallet, ethers) {
         console.log(`Verified ETH balance for ${wallet.address()}: 0`)
     } //end if-else{}
 } //end displayZkSyncBalance ()
+
+module.exports = {
+  getZkSyncProvider,
+  getEthereumProvider,
+  depositToZkSync,
+  registerAccount,
+  displayZkSyncBalance,
+  transfer,
+  withdrawToEthereum,
+  getFee,
+  initAccount
+} //end .exports()
