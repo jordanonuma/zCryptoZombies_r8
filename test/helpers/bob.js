0ee9c1d27106a0d8ee18b6b492df6eb54b0f2533
jordanonuma <jordanonuma@outlook.com>
2021-04-19 17:06:08 -1000

    Sets up Bob's zkSync wallet.

--- test/helpers/bob.js
+++ test/helpers/bob.js
@@ -7,7 +7,8 @@
     const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
     const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
 
-    const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider)
+    const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider) //sets up Bob's Rinkeby wallet
     console.log('Bob's Rinkeby address is: ${rinkebyWallet.address})')
     console.log('Bob's initial balance on Rinkeby is: ${ethers.utils.formatEther(await rinkebyWallet.getBalance())}')
+    const bobZkSync = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zkSync)
 })()
