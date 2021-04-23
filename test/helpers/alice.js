7613db2e15f14550bcb40cdef0344ff1251884e6
jordanonuma <jordanonuma@outlook.com>
2021-04-23 07:38:46 -1000

    Retrieves withdrawal fee and calls zksync function to withdraw.

--- test/helpers/alice.js
+++ test/helpers/alice.js
@@ -26,4 +26,8 @@
     console.log('Transferring')
     const transferFee = await utils.getFee('Transfer', aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
     await utils.transfer(aliceZkSyncWallet, process.env.BOB_ADDRESS, amountToTransfer, transferFee, token, zksync, ethers)
+
+    console.log('Withdrawing')
+    const wtihdrawalFee = utils.getFee('Withdraw', aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
+    await utils.withdrawToEthereum(aliceZkSyncWallet, amountToWithdraw, withdrawalFee, token, zksync, ethers)
   })()
