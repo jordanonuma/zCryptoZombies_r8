ccb3ded13951ac6a1bfa1defdcb6cc42e844f63c
jordanonuma <jordanonuma@outlook.com>
2021-05-02 08:40:33 -1000

    Replaced with tokenSet for stablecoin and its function formatToken for the fee.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -84,9 +84,9 @@ async function transfer (from, toAddress, amountToTransfer, transferFee, token, 
     console.log(transferReceipt)
 } //end function transfer ()
 
-async function getFee (transactionType, address, token, zkSyncProvider, ethers) {
+async function getFee (transactionType, address, token, zkSyncProvider, tokenSet) {
     const feeInWei = await zkSyncProvider.getTransactionFee(transactionType, address, token)
-    return ethers.utils.formatEther(feeInWei.totalFee.toString())
+    return ethers.utils.formatEther(token, tokenSet.formatToken(fee.totalFee))
 } //end function getFee ()
 
 async function withdrawToEthereum (wallet, amountToWithdraw, withdrawalFee, token, zksync, tokenSet) {
